5bfee7976c5d097df92081f5e2980b89867c47f7
deepakkalyani355 <deepak.kalyani@wmgamingstudioz.com>
2025-12-27 12:11:04 +0530

    update : DTO schema documentation added

--- src/main/java/com/eazybytes/accounts/dto/AccountsDTO.java
+++ src/main/java/com/eazybytes/accounts/dto/AccountsDTO.java
@@ -1,17 +1,30 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 @Data
 public class AccountsDTO {
     @Size(min = 10,max = 10,message = "Account no should be of 10 digit length")
+    @Schema(
+            description = "Account Number of Eazy Bank account", example = "3454433243"
+    )
     private long accountNumber;
 
     @NotEmpty(message = "Account type should not be null or empty")
+    @Schema(
+            description = "Account type of Eazy Bank account", example = "Savings"
+    )
     private String accountType;
 
     @NotEmpty(message = "Branch address type should not be null or empty")
+    @Schema(
+            description = "Eazy Bank branch address", example = "123 NewYork"
+    )
     private String branchAddress;
 }
--- src/main/java/com/eazybytes/accounts/dto/CustomerDTO.java
+++ src/main/java/com/eazybytes/accounts/dto/CustomerDTO.java
@@ -1,20 +1,36 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Data;
-
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
 @Data
 public class CustomerDTO {
+    @Schema(
+            description = "Name of the customer", example = "Eazy Bytes"
+    )
     @NotEmpty(message = "Name cannot be null or empty")
     private String name;
 
+    @Schema(
+            description = "Email address of the customer", example = "tutor@eazybytes.com"
+    )
     @NotEmpty(message = "Email cannot be null or empty")
     @Email(message = "Email should be valid email")
     private String email;
 
+    @Schema(
+            description = "Mobile Number of the customer", example = "9345432123"
+    )
     @Min(value = 1000000000L, message = "Mobile no should be 10 digits long")
     @Max(value = 9999999999L, message = "Mobile no should be 10 digits long")
     private int mobileNumber;
 
+    @Schema(
+            description = "Account details of the Customer"
+    )
     private AccountsDTO accountsDto;
 }
